2781bdff98deefae15816f96b219a1c1a94274ea
David Parry <david.parry@suranyami.com>
2020-03-04 12:20:21 +1100

    Well, this isnt working…

--- lib/matching_brackets.ex
+++ lib/matching_brackets.ex
@@ -1,9 +1,35 @@
 defmodule MatchingBrackets do
+
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t()) :: boolean
+  def check_brackets(""), do: true
+
   def check_brackets(str) do
-    true
+    str
+    |> String.to_charlist()
+    |> balanced()
   end
+  def balanced(''), do: true
+  def balanced([]), do: true
+  def balanced([?{ | tail]), do: balanced(tail) && closes_with(tail, ?})
+  def balanced([?[ | tail]), do: balanced(tail) && closes_with(tail, ?])
+  def balanced([?( | tail]), do: balanced(tail) && closes_with(tail, ?))
+  def balanced(_), do: true
+
+  def closes_with(str, close_char) do
+    balanced(str) &&
+    str
+    |> String.reverse()
+    |> closes_with(close_char)
+  end
+
+  def closes_with([?]], ?]), do: true
+  def closes_with([?}], ?}), do: true
+  def closes_with([?)], ?)), do: true
+
+  def closes_with([?] | _], ?]), do: true
+  def closes_with([?} | _], ?}), do: true
+  def closes_with([?) | _], ?)), do: true
 end
